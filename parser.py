e6556dd83db314ad8304740715ef2dc6b56e06b1
Beto Esquivel <ppbeto94@gmail.com>
2015-02-03 20:40:11 -0600

    Made some basic random tests, and parser passed.

--- parser.py
+++ parser.py
@@ -8,7 +8,8 @@ def p_programa(p):
     '''programa : PROGRAM ID ';' programa2 bloque'''
 
 def p_programa2(p):
-    '''programa2 : vars | empty'''
+    '''programa2 : vars
+                 | empty'''
 
 def p_vars(p):
     '''vars : VAR vars2'''
@@ -17,22 +18,28 @@ def p_vars2(p):
     '''vars2 : ID vars3'''
 
 def p_vars3(p):
-    '''vars3 : ',' vars2 | ':' tipo ';' vars4'''
+    '''vars3 : ',' vars2
+             | ':' tipo ';' vars4'''
 
 def p_vars4(p):
-    '''vars4 : vars2 | empty'''
+    '''vars4 : vars2
+             | empty'''
 
 def p_tipo(p):
-    '''tipo : INT | FLOAT'''
+    '''tipo : INT
+            | FLOAT'''
 
 def p_bloque(p):
     '''bloque : '{' bloque2 '}' '''
 
 def p_bloque2(p):
-    '''bloque2 : estatuto bloque2 | empty '''
+    '''bloque2 : estatuto bloque2
+               | empty '''
 
 def p_estatuto(p):
-    '''estatuto : asignacion | condicion | escritura'''
+    '''estatuto : asignacion
+                | condicion
+                | escritura'''
 
 def p_asignacion(p):
     '''asignacion : ID '=' expresion ';' '''
@@ -61,13 +68,16 @@ def p_escritura2(p):
                   | CTES escritura3'''
 
 def p_escritura3(p):
-    '''escritura3 : ',' escritura2 | empty '''
+    '''escritura3 : ',' escritura2
+    | empty '''
 
 def p_exp(p):
     '''exp : termino exp2'''
 
 def p_exp2(p):
-    '''exp2 : '+' exp | '-' exp | empty'''
+    '''exp2 : '+' exp
+            | '-' exp
+            | empty'''
 
 def p_termino(p):
     '''termino : factor termino2'''
@@ -75,14 +85,16 @@ def p_termino(p):
 def p_termino2(p):
     '''termino2 : '*' termino
                 | '/' termino
-                | empty'''
+                | empty '''
 
 def p_factor(p):
     '''factor : '(' expresion ')'
-              | factor2'''
+              | factor2 '''
 
 def p_factor2(p):
-    '''factor2: '+' varcte | '-' varcte | varcte'''
+    '''factor2 : '+' varcte
+               | '-' varcte
+               | varcte '''
 
 def p_varcte(p):
     '''varcte : ID
@@ -94,6 +106,6 @@ def p_empty(p):
     pass
 
 def p_error(p):
-    print "Error de sintaxis: ", p
+    print "Error de sintaxis en: '%s' ", p.value
 
 parser = yacc.yacc()
--- testingParser.py
+++ testingParser.py
@@ -1,5 +1,34 @@
 import parser
+import logging
 
+def test(code):
+    log = logging.getLogger()
+    parser.parser.parse(code, debug=log)
+
+print "Programa con 1 var y 1 asignacion bien: "
 s = "program id; var beto: int; { id = 1234; }"
-
-parser.parser.parse(s)
+test(s)
+print "Programa con 1 var mal: "
+s = "program ; var beto: int; { id = 1234; }"
+test(s)
+print "Programa sin vars bien: "
+s = "program id;  { id = 1234; }"
+test(s)
+print "Programa con var mal: "
+s = "program id; var beto int; { id = 1234; }"
+test(s)
+print "Programa con var mal: "
+s = "program id; var beto: int { id = 1234; }"
+test(s);
+print "Programa con var mal: "
+s = "program id; beto: int; { id = 1234; }"
+test(s)
+print "Programa con bloque vacio bien: "
+s = "program id; var beto: int; {  }"
+test(s)
+print "Programa con bloque lleno y estatuto mal: "
+s = "program id; var beto: int; { id = 1234; id2 = 12345 }"
+test(s)
+print "Programa con bloque lleno y condicion mal: "
+s = "program id; var beto: int; { id = 1234; if ( 8 > 3 ) { id3 = 34234; } else { } }"
+test(s)
